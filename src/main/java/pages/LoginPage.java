299020351e7b17366954d7f74018d64d5300a5a4
Rishikesh Tiwari <rishikesht775@gmail.com>
2025-06-22 20:46:01 +0530

    Updated LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,29 @@
+// LoginPage.java
 package pages;
 
-public class LoginPage {
+import com.microsoft.playwright.Page;
 
+public class LoginPage {
+    private Page page;
+    private final String emailInput = "#email";
+    private final String passwordInput = "#pass";
+    private final String loginButton = "#send2";
+
+    public LoginPage(Page page) {
+        this.page = page;
+    }
+
+    public void navigateTo(String url) {
+        page.navigate(url);
+    }
+
+    public void login(String email, String password) {
+        page.fill(emailInput, email);
+        page.fill(passwordInput, password);
+        page.click(loginButton);
+    }
+
+    public Page getPage() {
+        return page;
+    }
 }
