fab9cfe92a7aa7c1b7ec283c0f8186d28ff12f8d
Rishi Tiwari <rishi.tiwari@pearson.com>
2025-06-22 20:28:02 +0530

    Updated BaseTest.java

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,5 +1,19 @@
 package base;
 
-public class BaseTest {
+import com.microsoft.playwright.Page;
+import utils.PlaywrightFactory;
 
+public class BaseTest {
+    protected Page page;
+    protected PlaywrightFactory pf;
+
+    public Page initBrowser() {
+        pf = new PlaywrightFactory();
+        page = pf.initBrowser();
+        return page;
+    }
+
+    public void tearDown() {
+        if (pf != null) pf.close();
+    }
 }
