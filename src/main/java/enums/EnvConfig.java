260b2da0ea9f22dc93640e74900f5428a0eb5911
Rishikesh Tiwari <rishikesht775@gmail.com>
2025-06-22 20:40:56 +0530

    Updated EnvCofig.java

--- src/main/java/enums/EnvConfig.java
+++ src/main/java/enums/EnvConfig.java
@@ -1,5 +1,12 @@
 package enums;
 
-public class EnvConfig {
+public enum EnvConfig {
+    BASE_URL("https://magento.softwaretestingboard.com/"),
+    LOGIN_URL("https://magento.softwaretestingboard.com/customer/account/login/"),
+    VALID_USERNAME("testuser@example.com"),
+    VALID_PASSWORD("test123");
 
+    private final String value;
+    EnvConfig(String value) { this.value = value; }
+    public String getValue() { return value; }
 }
