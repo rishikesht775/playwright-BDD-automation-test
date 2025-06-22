c2b58b0b2841d54af21ff159360d2a2e4f3b4970
Rishikesh Tiwari <rishikesht775@gmail.com>
2025-06-22 20:58:55 +0530

    Updated Hooks.java

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -1,5 +1,18 @@
 package stepdefinitions;
 
-public class Hooks {
+import base.BaseTest;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
+public class Hooks extends BaseTest {
+
+    @Before
+    public void setUp() {
+        initBrowser();
+    }
+
+    @After
+    public void tearDownScenario() {
+        tearDown();
+    }
 }
